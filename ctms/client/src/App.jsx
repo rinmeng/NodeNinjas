8d3d959ef0e8d6cf6f1bb338b65e830934b973ac
EnockMutabazi <162542644+EnockMutabazi@users.noreply.github.com>
2025-02-01 19:53:18 -0800

    Added User Dashboard in Home page

--- ctms/client/src/App.jsx
+++ ctms/client/src/App.jsx
@@ -9,6 +9,7 @@ import Test from "./pages/Test";
 import NotFound from "./pages/NotFound";
 import "./css/output.css";
 
+
 function App() {
   return (
     <Router>
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+         
 
           {/*
             Nested routes for the testing pages
