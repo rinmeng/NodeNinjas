a01d89b64846bf895ca1b259f68bd30aad3d7e71
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-02-14 13:14:07 -0800

    Rearranged my UseEffect, if conditional and functions

--- ctms/client/src/pages/Admin.jsx
+++ ctms/client/src/pages/Admin.jsx
@@ -19,6 +19,10 @@ const Admin = ({ sessionUser, devMode }) => {
   // UseState for the list of tasks you can view
   const [taskList, setTaskList] = useState([]);
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   if ((!sessionUser || sessionUser.role !== "admin") && !devMode) {
     return (
       <div className="mp5 my-16 animate-fadein">
@@ -32,6 +36,21 @@ const Admin = ({ sessionUser, devMode }) => {
       </div>
     );
   }
+
+  //Here is my data for the table which view all created tasks under Managing Roles.
+  const columns = [
+    { header: "Task Name", key: "name" },
+    { header: "Task Description", key: "description" },
+    { header: "Task Priority", key: "priority" },
+    { header: "Due Date", key: "date" },
+  ];
+
+  const usersColumns = [
+    { header: "User Id", key: "id" },
+    { header: "Username", key: "username" },
+    { header: "Email Address", key: "email" },
+    { header: "Role", key: "role" },
+  ];
 
   //Our function will check the new task that is added when the user presses the "Add" button on the Add Task form
   const HandleAddandViewTask = () => {
@@ -54,21 +73,6 @@ const Admin = ({ sessionUser, devMode }) => {
     setTaskDue("");
   };
 
-  //Here is my data for the table which view all created tasks under Managing Roles.
-  const columns = [
-    { header: "Task Name", key: "name" },
-    { header: "Task Description", key: "description" },
-    { header: "Task Priority", key: "priority" },
-    { header: "Due Date", key: "date" },
-  ];
-
-  const usersColumns = [
-    { header: "User Id", key: "id" },
-    { header: "Username", key: "username" },
-    { header: "Email Address", key: "email" },
-    { header: "Role", key: "role" },
-  ];
-
   //If the user chooses a specific option from the selector, this function will be called and sort our tasks based on their priority or due date
   const FilterDataByOption = () => {
     const clonedList = [...taskList];
@@ -78,10 +82,6 @@ const Admin = ({ sessionUser, devMode }) => {
 
     return clonedList;
   };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
 
   const fetchUsers = () => {
     fetch(proxy + "user/all", { credentials: "include" })
@@ -234,16 +234,6 @@ const Admin = ({ sessionUser, devMode }) => {
                 <option value="inProgress">In Progress</option>
                 <option value="completed">Completed</option>
               </select>
-            </div>
-          </div>
-
-          {/*Buttom for filtering the task by date in view task section*/}
-          <div className="mt-4 flex justify-end">
-            <div className="bg-sky-700 inline-block p-4 rounded-xl mr-5">
-              <label className="text-xl mt-15">Filter From this date:</label>
-              <input type="date" className="mt-15 ml-5 bg-blue-900"></input>
-              <label className="text-xl mt-15"> To this date:</label>
-              <input type="date" className="mt-15 ml-5 bg-blue-900"></input>
             </div>
           </div>
 
