ee6bd5182a3124dffb321b284c8d744313f21b37
KoesOremus <dibaburks@gmail.com>
2025-03-06 16:03:32 -0800

    Update dependencies and improve App component tests

--- ctms/client/src/App.test.js
+++ ctms/client/src/App.test.js
@@ -1,19 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders app without crashing', () => {
-  // Mocking the sessionUser data since it's used in your App component
-  const mockSessionUser = null;
-  
-  // Wrap App in BrowserRouter for testing
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  
-  // Test for something that should be in your app when loaded
-  // You can adjust this based on what you expect to see in your app
-  expect(document.body).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
