fd3145316b0a5a57142cf118a512bd5ad962e18f
rinwindows <phurinmeng@gmail.com>
2025-03-08 12:46:42 -0800

    Clean up imports in Datatable and update CSS path in index.jsx

--- ctms/client/src/index.jsx
+++ ctms/client/src/index.jsx
@@ -1,11 +1,11 @@
 // src/index.js
 import React from "react";
-import ReactDOM from "react-dom/client"; // Import from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./css/App.css";
+import "@/src/css/App.css";
 
 const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement); // Create a root
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
