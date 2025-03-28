c1f7b51518eb990fc274b2b13362d62bc418d01e
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-03-28 14:13:32 -0700

    Notifications are now sorted based on their status which is either read or unread

--- ctms/client/utils/NotificationProvider.jsx
+++ ctms/client/utils/NotificationProvider.jsx
@@ -14,7 +14,6 @@ export function NotificationProvider({ children }) {
   const [notificationsNeedRefetch, setNotificationsNeedRefetch] =
     useState(false);
   const [notificationToAdd, setNotificationToAdd] = useState("");
-  const [notificationSort, setNotificationSort] = useState([]);
 
   // Fetch notifications when user changes
   useEffect(() => {
@@ -27,21 +26,10 @@ export function NotificationProvider({ children }) {
 
   // Refetch notifications when needed
   useEffect(() => {
-    if (notifications.length > 0) {
-      notificationSort = [...notifications].sort((a, b) => {
-        if (a.status === "unread" && b.status === "read") {
-          return -1;
-        }
-        if (a.status === "read" && b.status === "unread") {
-          return 1;
-        }
-        return new Date(b.created_at) - new Date(a.created_at);
-      });
-      setNotifications(notificationSort);
-    } else {
-      setNotifications([]);
+    if (notificationsNeedRefetch) {
+      fetchNotifications();
     }
-  }, [notifications]);
+  }, [notificationsNeedRefetch]);
 
   // Send notification when notificationToAdd changes
   useEffect(() => {
