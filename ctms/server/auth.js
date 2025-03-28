af12562d9f4373b00306050ea7fb9435cdc93c11
rin! <phurinmeng@gmail.com>
2025-03-27 19:08:07 -0700

    Refactor authentication checks to improve code clarity and maintainability

--- ctms/server/auth.js
+++ ctms/server/auth.js
@@ -1,8 +1,16 @@
 const devMode = false;
 
+// Base authentication check
+const checkAuthentication = (req) => {
+    if (devMode) {
+        return true;
+    }
+    return !!req.session?.user;
+};
+
 // For normal authentication, we check if the user is authenticated
 const isAuthenticated = (req, res, next) => {
-    if (!req.session?.user && !devMode) {
+    if (!checkAuthentication(req)) {
         return res.status(401).json({ message: 'User is not authenticated' });
     }
     next();
@@ -11,15 +19,13 @@ const isAuthenticated = (req, res, next) => {
 // For admin authentication, we check if the user is authenticated and has the admin role
 const isAuthAsAdmin = (req, res, next) => {
     // First check if user is authenticated
-    if (!devMode) {
-        if (!req.session?.user) {
-            return res.status(401).json({ message: 'User is not authenticated' });
-        }
+    if (!checkAuthentication(req)) {
+        return res.status(401).json({ message: 'User is not authenticated' });
+    }
 
-        // Then check if user has admin role if not, return 403 Forbidden
-        if (req.session.user.role !== 'admin') {
-            return res.status(403).json({ message: 'Admin access is required to see this page' });
-        }
+    // Then check if user has admin role if not, return 403 Forbidden
+    if (!devMode && req.session.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Admin access is required to see this page' });
     }
 
     next();
