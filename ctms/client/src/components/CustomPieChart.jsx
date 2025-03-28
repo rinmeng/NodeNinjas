99491336726bb09dc601464c48f9d99f38373a82
EnockMutabazi <162542644+EnockMutabazi@users.noreply.github.com>
2025-03-28 11:26:44 -0700

    No code changes made

--- ctms/client/src/components/CustomPieChart.jsx
+++ ctms/client/src/components/CustomPieChart.jsx
@@ -40,7 +40,6 @@ export function CustomPieChart({
     ...(display && { [dataType]: display }),
   };
 
-  //testing
 
   useEffect(() => {
     fetch(`${proxy}/task/assignedto/manager/${user.id}`, {
