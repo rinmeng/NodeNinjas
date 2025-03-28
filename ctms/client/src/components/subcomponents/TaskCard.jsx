ed2bc9d4f4eed56ab1baece4b63df94cb2277180
rin-windows <phurinmeng@gmail.com>
2025-03-27 21:11:26 -0700

    Remove console.log statement from TaskCard component

--- ctms/client/src/components/subcomponents/TaskCard.jsx
+++ ctms/client/src/components/subcomponents/TaskCard.jsx
@@ -213,7 +213,6 @@ const TaskCard = ({ task, user, setNeedsRefetch, devMode }) => {
         });
       })
       .catch((err) => {
-        console.log(err);
         setFeedbackMessage({
           title: "Error",
           description: `Failed to ${isTaskLocked ? "unlock" : "lock"} task.`,
