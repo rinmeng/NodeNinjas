ba2c94fc204fe836e27742c3d59443392563bca8
rin! <phurinmeng@gmail.com>
2025-03-28 17:31:45 -0700

    Redirect home route to setup page instead of proxy

--- ctms/server/routes/home.js
+++ ctms/server/routes/home.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const pool = require("../db"); // Database connection
 const router = express.Router();
-const proxy = "http://localhost:13000";
 
 router.get("/", (req, res) => {
-  res.redirect(`${proxy}/docs`);
+  res.redirect(`/setup`);
 });
 
 module.exports = router;
